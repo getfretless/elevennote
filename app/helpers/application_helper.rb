177e521bec2b114fecb43cf4c1608c68a463594d
Dave Strus <dave@davestrus.com>
2014-11-13 17:45:17 -0500

    Create notes table. Add layout and css.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_messages
+    flash.map do |key, message|
+      alert_type = case key
+        when 'notice' then 'alert-success'
+        when 'alert'  then 'alert-error'
+        else nil
+      end
+      content_tag :p, message, id: 'flash', class: ['alert', alert_type].join(' ')
+    end.join.html_safe
+  end
 end
--- db/migrate/20141113223653_create_notes.rb
+++ db/migrate/20141113223653_create_notes.rb
@@ -1,6 +1,11 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
+      t.string :title
+      t.text :body_html
+      t.text :body_text
+      t.references :user, index: true
+      t.timestamps
     end
   end
 end
