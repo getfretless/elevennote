07370154492fafb1de37bc5275bfd1cac4753c38
David Jones <unixmonkey1@gmail.com>
2014-12-10 11:25:26 -0500

    Allow Cross-Origin Request Sharing

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,6 @@
 class API::APIController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :set_access_control_headers
 
   protected
 
@@ -13,5 +14,12 @@ class API::APIController < ApplicationController
 
   def current_api_user
     @current_api_user ||= User.find_by(api_key: params[:api_key]) if params[:api_key]
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
