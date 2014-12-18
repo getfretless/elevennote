4989ecb1de6d99afb21ae664c9cfcf3354b43cfb
David Jones <unixmonkey1@gmail.com>
2014-12-18 01:06:55 -0500

    Send full user for api login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class API::V1::SessionsController < API::APIController
   def create
     @user = User.find_by username: user_params[:username]
     if @user.present? && @user.authenticate(user_params[:password])
-      render json: { notice: t('session.flash.create.success'), api_key: @user.api_key }
+      render
     else
       render json: { error: t('session.flash.create.failure'), status: :unprocessable_entity }
     end
